03b39a33b49d7306e3424f82b3a7d95c2910dd3d
Nuh Yamin <nuhyamin31@gmail.com>
2025-03-11 01:18:06 +0700

    Small style improvement in student Main Page

--- client/src/components/dashboard/SessionsSection.jsx
+++ client/src/components/dashboard/SessionsSection.jsx
@@ -54,7 +54,7 @@ const SessionsSection = ({ title, sessions, type }) => {
       <h3 className="text-2xl font-bold text-gray-800 mb-6 flex items-center">
         <span className="relative">
           {title}
-          <div className="absolute -bottom-2 left-0 h-1 w-12 bg-blue-400 rounded"></div>
+          
         </span>
       </h3>
       
